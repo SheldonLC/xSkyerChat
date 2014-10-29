b8edd58550d520e7f6a22afe40b6de696276d872
Yin Bo <yinbo@YindeMacBook-Pro.local>
2014-10-29 14:15:23 +0800

    Initial Commit

---
+++ XSkyerChatroom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XSkyerChatroom
+//
+//  Created by Yin Bo on 14/10/29.
+//  Copyright (c) 2014年 SheldonLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XSkyerChatroom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XSkyerChatroom
+//
+//  Created by Yin Bo on 14/10/29.
+//  Copyright (c) 2014年 SheldonLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
